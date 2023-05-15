1a3674a8c22af79f11d91bd0f10fbca92731bdcc
EddyGlezz <eddyglezz95@gmail.com>
2023-05-15 10:23:59 +0200

    the bookings will be a json with start and finish time

--- include/booking.hpp
+++ include/booking.hpp
@@ -7,49 +7,28 @@
 #include<QJsonValue>
 #include<QJsonObject>
 #include<set>
-class Booking
+class Booking : public QJsonObject
 {
 	public:
-		Booking(const QDateTime start,const QDateTime finish);
-        Booking(QDataStream &buffer, bool with_passcode=false);
-        QJsonObject get_jsob(void)const{
-            QJsonObject var;
-            var.insert("start",m_start.toString());
-            var.insert("finish",m_finish.toString());
-            var.insert("code_str",code_str_);
-            return var;
-        }
+    using QJsonObject::QJsonObject;
+    Booking(const QJsonObject &var=QJsonObject()):QJsonObject(var){};
 		std::vector<QDate> get_days(void)const;
 		std::vector<int> get_hours(const QDate& day)const;
 		bool check_validity(const QDateTime & ref)const;
-        QString code_str(void)const{return code_str_;};
-		void set_code_str(QString code);
-        QByteArray get_passcode(void)const{return m_passcode;}
-		bool verify_code_str(QString code_str)const;
-		QDateTime start(void)const{return m_start;}
-		QDateTime finish(void)const{return m_finish;}
-		void set_start(QDateTime start_){m_start=start_;}
-		void set_finish(QDateTime finish_){m_finish=finish_;}
+
 		friend bool operator < (const Booking& b1, const Booking& b2)
 		{
-			return(b1.m_start<b2.m_start&&b1.m_finish<b2.m_start);
+            return(b1["start"].toInteger()<b2["start"].toInteger()&&b1["finish"].toInteger()<b2["finish"].toInteger());
 		}
 		friend bool operator < (const Booking& b1, const QDateTime& b2)
 		{
-			return(b1.m_finish<b2);
+            return(QDateTime::fromSecsSinceEpoch(b1["finish"].toInteger())<b2);
 		}
-        void serialize(QDataStream &buffer,bool with_passcode) const;
-		quint64 calculate_price(quint64 per_hour)const;
-        static std::tuple<std::set<Booking>, quint64, QByteArray> deserialize_state(QByteArray &state);
-        static QByteArray serialize_state(const std::set<Booking>& books, const quint64&price_per_hour_, const QByteArray &pay_to_address_hash);
-        static Booking get_new_booking_from_metadata(QByteArray& metadata);
-        static QByteArray create_new_bookings_metadata(Booking &book);
-signals:
-		void state_changed(void);
-	private:
 
-        QDateTime m_start, m_finish;
-		QByteArray m_passcode;   //32 byte
-        QString code_str_;
+		quint64 calculate_price(quint64 per_hour)const;
+
+        static Booking get_new_booking_from_metadata(const QByteArray& metadata);
+        static QByteArray create_new_bookings_metadata(Booking &book);
+
 };
 
--- src/booking.cpp
+++ src/booking.cpp
@@ -1,77 +1,19 @@
 #include"booking.hpp"
 #include <QJsonObject>
-#include <QDataStream>
-#include <QCryptographicHash>
-#include<QIODevice>
-Booking::Booking(const QDateTime start,const QDateTime finish):
-    m_start(start),m_finish(finish),code_str_("")
-{
+#include <QJsonDocument>
 
-};
 
-void Booking::set_code_str(QString code_str)
-{
-
-    code_str_=code_str;
-    const auto code_m=static_cast<quint64>(code_str.toULongLong());
-    QByteArray var;
-    auto buffer=QDataStream(&var,QIODevice::WriteOnly | QIODevice::Append);
-    buffer.setByteOrder(QDataStream::LittleEndian);
-
-    buffer<<m_start.toMSecsSinceEpoch()<<m_finish.toMSecsSinceEpoch()<<code_m;
-    m_passcode=QCryptographicHash::hash(var,QCryptographicHash::Blake2b_256);
-}
-bool Booking::verify_code_str(QString code_str)const
-{
-    quint64 V_code_m=static_cast<quint64>(code_str.toULongLong());
-
-    QByteArray var;
-    auto buffer=QDataStream(&var,QIODevice::WriteOnly | QIODevice::Append);
-    buffer.setByteOrder(QDataStream::LittleEndian);
-    buffer<<m_start.toMSecsSinceEpoch()<<m_finish.toMSecsSinceEpoch()<<V_code_m;
-
-    auto V_passcode=QCryptographicHash::hash(var,QCryptographicHash::Blake2b_256);
-
-    return(V_passcode==m_passcode);
-}
-
-Booking::Booking(QDataStream &buffer, bool with_passcode):code_str_("")
-{
-
-    m_start=QDateTime();
-    quint64 st,fi;
-    buffer>>st>>fi;
-
-    m_start.setMSecsSinceEpoch(st);
-    m_finish.setMSecsSinceEpoch(fi);
-
-    if(with_passcode)
-    {
-        m_passcode=QByteArray(32,0);
-        buffer.readRawData(m_passcode.data(),32);
-    }
-
-}
-void Booking::serialize(QDataStream &buffer,bool with_passcode) const
-{
-
-    buffer<<m_start.toMSecsSinceEpoch()<<m_finish.toMSecsSinceEpoch();
-    if(with_passcode)
-    {
-        buffer.writeRawData(m_passcode.data(),32);
-    }
-}
 bool Booking::check_validity(const QDateTime & ref)const
 {
-    if(ref<m_finish)return true;
+    if(ref<QDateTime::fromSecsSinceEpoch((*this)["finish"].toInteger()))return true;
 
     return false;
 }
 std::vector<QDate> Booking::get_days(void)const
 {
     std::vector<QDate> days;
-    auto startday=m_start.date();
-    const auto finishday=m_finish.date();
+    auto startday=QDateTime::fromSecsSinceEpoch((*this)["start"].toInteger()).date();
+    const auto finishday=QDateTime::fromSecsSinceEpoch((*this)["finish"].toInteger()).date();
     days.push_back(startday);
 
     for(auto i=0;i<startday.daysTo(finishday);i++)
@@ -86,8 +28,10 @@ std::vector<int> Booking::get_hours(const QDate& day)const
 {
     std::vector<int> booked_hours;
 
-    auto st=(day==m_start.date())?m_start.time():QTime(0,0);
-    auto fi=(day==m_finish.date())?m_finish.time():QTime(23,59,59,59);
+    auto st=(day==QDateTime::fromSecsSinceEpoch((*this)["start"].toInteger()).date())?
+                QDateTime::fromSecsSinceEpoch((*this)["start"].toInteger()).time():QTime(0,0);
+    auto fi=(day==QDateTime::fromSecsSinceEpoch((*this)["finish"].toInteger()).date())?
+                QDateTime::fromSecsSinceEpoch((*this)["finish"].toInteger()).time():QTime(23,59,59,59);
     auto interval=(st.secsTo(fi)+1)/60/60;
     for(auto i =0;i<interval;i++)
     {
@@ -98,71 +42,23 @@ std::vector<int> Booking::get_hours(const QDate& day)const
 
 quint64 Booking::calculate_price(quint64 per_hour)const
 {
-    const auto hours=(m_start.secsTo(m_finish)+1)/60/60;
+    const auto hours=((*this)["finish"].toInteger()-(*this)["start"].toInteger()+1)/60/60;
     return per_hour*hours;
 }
-QByteArray Booking::serialize_state(const std::set<Booking>&books, const quint64&price_per_hour_, const QByteArray &pay_to_address_hash)
+
+
+
+Booking Booking::get_new_booking_from_metadata(const QByteArray &metadata)
 {
-    QByteArray var;
-    auto buffer=QDataStream(&var,QIODevice::WriteOnly | QIODevice::Append);
-    buffer.setByteOrder(QDataStream::LittleEndian);
-    buffer<<static_cast<quint16>(books.size());
-    for(auto& v:books)
-    {
-        v.serialize(buffer,0);
-    }
-    buffer<<price_per_hour_;
-    buffer.writeRawData(pay_to_address_hash,32);
-    return var;
+    const auto var=QJsonDocument::fromJson(metadata).object();
+    if(!var["start"].isUndefined()&&!var["finish"].isUndefined()&&
+            var["start"].toInteger()>0&&var["finish"].toInteger()>0&&
+            var["finish"].toInteger()>var["start"].toInteger())
+        return Booking(var);
 
-}
-
-std::tuple<std::set<Booking>, quint64, QByteArray> Booking::deserialize_state(QByteArray& state)
-{
-    std::set<Booking> var;
-    auto pay_to_address_hash=QByteArray(32,0);
-    quint64 price;
-    if(state.size()>=42)
-    {
-        auto buffer=QDataStream(&state,QIODevice::ReadOnly);
-        buffer.setByteOrder(QDataStream::LittleEndian);
-        quint16 nbooks;
-        buffer>>nbooks;
-        if(state.size()==42+nbooks*16)
-        {
-            for (auto i=0;i<nbooks;i++)
-            {
-                const auto b=Booking(buffer,0);
-                if(b.finish()>QDateTime::currentDateTime()&&b.finish().date()<QDate::currentDate().addDays(7))
-                {
-                    var.insert(b);
-                }
-
-            }
-
-            buffer>>price;
-            buffer.readRawData(pay_to_address_hash.data(),32);
-
-        }
-    }
-
-    return {var,price,pay_to_address_hash};
-}
-Booking Booking::get_new_booking_from_metadata(QByteArray& metadata)
-{
-    if(metadata.size()==48)
-    {
-        auto buffer=QDataStream(&metadata,QIODevice::ReadOnly);
-        buffer.setByteOrder(QDataStream::LittleEndian);
-        return Booking(buffer,1);
-    }
-    return Booking(QDateTime::fromMSecsSinceEpoch(0),QDateTime::fromMSecsSinceEpoch(0));
+    return Booking();
 }
 QByteArray Booking::create_new_bookings_metadata(Booking &book)
 {
-    QByteArray var;
-    auto buffer=QDataStream(&var,QIODevice::WriteOnly | QIODevice::Append);
-    buffer.setByteOrder(QDataStream::LittleEndian);
-    book.serialize(buffer,1);
-    return var;
+    return QJsonDocument(book).toJson();
 }
